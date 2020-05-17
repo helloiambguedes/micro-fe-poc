b95d0141632dd332e89c803883ede5096ec3e3b1
Bruno Guedes <bruno.guedes@mozantech.com>
2020-05-17 14:09:49 +0100

    added checkpoint and fixed typo

--- reactApp/app.jsx
+++ reactApp/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => {
   console.log("rendering react app");
-  return <h1 style={{ color: "blue" }}>This is a react application</h1>;
+  return <h1 style={{ color: "blue" }}>This is a React application</h1>;
 };
 
 export default App;
