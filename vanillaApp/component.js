65cbc29cb06679162c929e03de5376dddbe59af1
Bruno Guedes <bguedes@DESKTOP-QUHK9F1.localdomain>
2020-05-29 23:22:53 +0100

    added get request to vanilla app

--- vanillaApp/component.js
+++ vanillaApp/component.js
@@ -1,3 +1,15 @@
-const VanillaComponent = "<h1>This is a vanilla app (already processed)</h1>";
+import axios from "axios";
+
+window.fetchRepos = () => {
+    axios('https://api.github.com/search/repositories?q=language:javascript')
+        .then(({ data }) => {
+            console.log(data.items);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+const VanillaComponent = "<h1>This is a vanilla app</h1><button onClick='window.fetchRepos()'>FETCH GITHUB REPOS</button>";
 
 export default VanillaComponent;
