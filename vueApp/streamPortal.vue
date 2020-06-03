3a54af1fae96aad1a16e8965b1617d48627cf411
Bruno Guedes <bruno.guedes@mozantech.com>
2020-06-03 16:47:44 +0100

    live coding setup

--- vueApp/streamPortal.vue
+++ vueApp/streamPortal.vue
@@ -1,5 +1,8 @@
 <template>
-  <iframe :src="url" style="width: 100%; height: 800px; border: none;" />
+  <section>
+    <iframe :src="url" style="width: 100%; height: 800px; border: none;" />
+    <button v-on:click="onClick">UPDATE IFRAME SRC</button>
+  </section>
 </template>
 
 <script>
@@ -8,7 +11,15 @@ import eventManager from "../utils/eventManager";
 export default {
   name: "streamPortal",
   data() {
-    return { url: "https://mozantech.com" };
+    return {
+      url: "https://mozantech.com",
+      streamUrl: "https://www.youtube.com/embed/e1-rr969y4U",
+    };
+  },
+  methods: {
+    onClick() {
+      this.url = this.streamUrl;
+    },
   },
   mounted() {
     eventManager.addEvent("UPDATE_VUE_IFRAME", (newUrl) => {
